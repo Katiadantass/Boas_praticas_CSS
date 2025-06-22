acb1a6366c18b8f169ba1855c5632649fb4a4f89
Katiadantass <katiadantas.2508@gmail.com>
2025-06-22 16:41:00 -0300

    commit inicial

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// scripts/script.js
+
+/*
+    Arquivo destinado a interações JavaScript do projeto "Exercício boas práticas de CSS".
+
+    Este projeto está estruturado usando a metodologia BEM para organização do CSS,
+    garantindo um código modular, legível e fácil de manter.
+
+    No momento, não há funcionalidades JS implementadas, mas este arquivo está preparado
+    para futuras implementações sem impactar a estrutura atual.
+*/
